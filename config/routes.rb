1dbfafd2ef855452c1dcdc72101500903475d00f
tstorey77 <tsstorey13@gmail.com>
2020-03-11 10:22:45 -0500

    added more nav items and added show pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
   get 'pages/skip-assets'
   resources :poketypes
   resources :items
-  resources :moves
   resources :ptypes
   resources :pokedexes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'search', to: 'pokedexes#search', as: 'search'
   get 'about', to: 'pages#about', as: 'about'
+  get 'ptype', to: 'ptypes#index', as: 'type'
+  get 'move', to: 'moves#index', as: 'moves'
+
   root to: 'pokedexes#index'
 end
